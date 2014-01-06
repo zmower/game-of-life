bbac35ca33d8ef34153eb9f5d9ce70379ae63a30
Chris M Moore <zmower@ntlworld.com>
2014-01-06 23:49:41 +0000

    Added cursor-based setup/modding

--- terminal_life.adb
+++ terminal_life.adb
@@ -4,18 +4,9 @@ use Terminal_Interface.Curses;
 
 procedure Terminal_Life is
 
-   procedure Populate (T : in out Life.Table) is
-      use Life;
-   begin
-      Clear (T);
-      for I in Index'range loop
-         for J in Index'range loop
-            if I + J mod 7 = 0 or I mod 3 = 0 then
-               Set (T, I, J);
-            end if;
-         end loop;
-      end loop;
-   end Populate;
+   -- Cursor position in table
+   X, Y : Life.Index := 0;
+   Draw_Cursor : Boolean := True;
 
    procedure Print (W : Window; T : Life.Table) is
       use Life;
@@ -23,28 +14,61 @@ procedure Terminal_Life is
       Clear (W);
       for I in Index'range loop
          for J in Index'range loop
-            if Get (T, I, J) = Alive then
-               Move_Cursor (W, Line_Position (I), Column_Position (J));
-               Text_IO.Put (W, 'X');
+            if Get (T, X => I, Y => J) = Alive then
+               Move_Cursor (W, Line_Position (J), Column_Position (I));
+               Text_IO.Put (W, '0');
             end if;
          end loop;
       end loop;
+      if Draw_Cursor then
+        Move_Cursor (W, Line_Position (Y), Column_Position (X));
+        Text_IO.Put (W, 'X');
+      end if;
       Refresh (W);
    end Print;
 
    Current : Life.Table;
    Iteration : constant := 40;
-   ctrl_Z : constant := 26;
+   ctrl_Z : constant Real_Key_Code := 26;
+   Key : Key_Code;
+   use Life;
 
 begin
-   Populate (Current);
+
+   Clear (Current);
+
    Init_Screen;
---   for i in 1  .. Iteration loop
+
    -- Press ctrl-Z to exit or any other key to advance.
-   while Get_Keystroke /= ctrl_Z loop
+   loop
+
       Print (Standard_Window, Current);
---      delay 1.0;
-      Life.Tick (Current);
+
+      Key := Get_Keystroke;
+      case Key is
+        when ctrl_Z => exit;
+        when Key_Cursor_Up | Character'Pos ('w') =>
+          Draw_Cursor := True;
+          Y := Y - 1;
+        when Key_Cursor_Down | Character'Pos ('s') =>
+          Draw_Cursor := True;
+          Y := Y + 1;
+        when Key_Cursor_Left | Character'Pos ('a') =>
+          Draw_Cursor := True;
+          X := X - 1;
+        when Key_Cursor_Right | Character'Pos ('d') =>
+          Draw_Cursor := True;
+          X := X + 1;
+        when Character'Pos (' ') =>
+          Draw_Cursor := False;
+          Life.Set (Current, X, Y);
+        when others =>
+          Draw_Cursor := False;
+          Life.Tick (Current);
+      end case;
+
    end loop;
+
    End_Windows;
+
 end Terminal_Life;
